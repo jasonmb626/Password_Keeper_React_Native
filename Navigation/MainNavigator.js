acdcf5a513106a551d2acf83aa7dce49bfb2d9a2
Jason Brunelle <jason@jasonbrunelle.com>
2019-11-18 11:04:36 -0600

    HeaderButtons replaced custom buttons

--- Navigation/MainNavigator.js
+++ Navigation/MainNavigator.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import { TouchableNativeFeedback } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { Ionicons } from '@expo/vector-icons';
+
+import HeaderRight from '../Components/HeaderRight';
 
 import PasswordListScreen from '../Screens/PasswordListScreen';
 
 const MainNavigator = createStackNavigator({
-    PasswordList: {
-        screen: PasswordListScreen,
-        navigationOptions: {
-            title: 'All Passwords',
-            headerRight: () => (
-                <TouchableNativeFeedback
-                  onPress={() => alert('This is a button!')}
-                  title="Info"
-                  color="#fff"
-                >
-            <Ionicons
-              name='md-add-circle-outline'
-              size={23}
-            />
-            </TouchableNativeFeedback>
-          )
-        }}});
+  PasswordList: {
+    screen: PasswordListScreen,
+    navigationOptions: {
+      title: 'All Passwords',
+      headerRight: () => (<HeaderRight />)
+    }
+  }
+});
     
-
-
 export default createAppContainer(MainNavigator);
